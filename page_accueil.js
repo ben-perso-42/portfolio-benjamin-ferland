c459a2780d24be83facb1447b08d1dbadead0762
Benjamin4253 <142919065+Benjamin4253@users.noreply.github.com>
2025-09-30 23:35:28 -0400

    Fini à 50% design page accueil
    Finit swiper, carrousel, navbar, flip-card

---
+++ page_accueil.js
@@ -0,0 +1,69 @@
+// Crée le swiper compétences
+const competencesSwiper = new Swiper('.competences-swiper', {
+    direction: 'horizontal', // Ça défile à l’horizontale
+    slidesPerView: 3,       // 3 slides visibles
+    spaceBetween: 30,       // Espace entre chaque slide
+    scrollbar: {
+        el: '.swiper-scrollbar', // Barre de scroll
+        draggable: true,         // On peut la bouger
+    },
+    breakpoints: {
+        320: { slidesPerView: 2, spaceBetween: 20 },   // Petit écran
+        768: { slidesPerView: 3, spaceBetween: 30 },   // Tablette
+        1024: { slidesPerView: 5, spaceBetween: 40 }   // Grand écran
+    }
+});
+
+
+
+
+/*Exemples Prof https://tim-montmorency.com/timdoc/582-424MO/swiper/swiper/ */
+
+/*Exemple 1*/
+/*const swiper = new Swiper('.swiper', {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+})*/
+
+
+/*Exemple 2*/
+/*const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});*/
